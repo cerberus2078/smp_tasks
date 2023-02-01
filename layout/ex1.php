bc661fade1f40ac844b4948e5d9d9a698f981450
Sara-Sofia Paananen <111874242+cerberus2078@users.noreply.github.com>
2023-02-01 11:08:10 +0200

    added task 3.4 and starting 3.5

--- layout/ex1.php
+++ layout/ex1.php
@@ -10,6 +10,16 @@
     ?>
     <h1> 3.3 Write the PHP code in to display the current date </h1>
     <?php echo date("d.m.Y") ?>
+
+    <h1> 3.4 $title = "PHP is interesting”. Put this variable as a title marked as h1 (heading 1) in your HTML document. </h1>
+    <?php
+    $title1 = "PHP is interesting";
+    echo "<h1>" . $title1 . "</h1>";
+    ?>
+
+    <h1> 3.5 $g1=5, $g2=4, $g3=5. These are the grades for 3 students in the course. Use HTML table into echo and include 3 columns S.n., Name, and grade. Your table should look like the one below: </h1>
+    
+
     <?php include "footer.php" ?>
 
 
