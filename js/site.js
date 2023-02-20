ae88baa9206cc3d987e59437e4a9af0e47f90390
Sara-Sofia Paananen <111874242+cerberus2078@users.noreply.github.com>
2023-02-20 11:27:26 +0200

    20.02 lecture done

---
+++ js/site.js
@@ -0,0 +1,18 @@
+// a simple function
+function hello(){
+    alert("Welcome To My Website")
+}
+// a simple function to add 2 numbers
+function add(){
+    document.write(5+6);
+}
+//document getElementById(id)
+
+//display
+
+//function to change the background color of the page
+function changeColor(where, newColor){
+    if(where == "background"){
+        document.body.style.backgroundColor = newColor;
+    }
+}
--- layout/ex1.php
+++ layout/ex1.php
@@ -12,6 +12,40 @@
     <h3> 3.3 Write the PHP code in to display the current date </h3>
     <?php echo date("d.m.Y") ?>
 
+                <script>
+                    document.write("Hello world this is JavaScript")
+                </script>
+                <noscript>
+                    Please enable JavaScript to view this content
+                </noscript>
+                <input type="button" onclick="hello()" value="Click Me">
+
+                <script>
+                //windows alert box
+                //window.alert("This will trigger an alert box");
+                //window.alert(5+10);
+                //document.write 
+                document.write("Hello this is interesting");
+                </script>
+
+                <button onclick="add()"> Click to Add </button>
+
+                <hr>
+                <p id="place1" style="color:purple; background-color:aqua;"></p>
+                <span id="place2" style="color:purple; background-color:beige;"></span>
+                <script>
+                    document.getElementById("place1").innerHTML = "This will go to place1";
+                    document.getElementById("place2").innerHTML = "This will go to place2";
+                </script>
+                <form>
+                <input type="color" name="background">
+                </form>
+
+                <h2> Changing background-color </h2>
+                <form>
+                <input type="color" name="background" onchange="changeColor('background', this.value)">
+                </form>
+
     <h3> 3.4 $title = "PHP is interesting”. Put this variable as a title marked as h1 (heading 1) in your HTML document. </h3>
     <?php
     $title1 = "PHP is interesting";
